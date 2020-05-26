d7cf07925280790e159eefb70d3988675de47e73
emark1 <devel010@umn.edu>
2020-05-25 22:01:02 -0500

    First

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;    
+
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x/10f) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z/10f) * gridSize;
+        transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+    }
+}
